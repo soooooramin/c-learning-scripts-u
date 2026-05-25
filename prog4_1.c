969c41ab57eaa72b472d51f7bfa075c437a789b0
soooooramin <mdaggss0@gmail.com>
2026-05-25 16:53:39 +0900

    Edit diffirent answers

--- prog4_1.c
+++ prog4_1.c
@@ -4,10 +4,10 @@ int main(void)
     float bekijou;
     int i;
     bekijou = 1;
-    for( i=1; i<=39; i++)
+    for( i=1; i<=256; i++)
     {
-    bekijou = bekijou * 2;
-    printf("%2d: %f \n", i, bekijou);
+    	bekijou = bekijou * 2;
+    	printf("%2d: %f \n", i, bekijou);
     }
     return 0;
 }
